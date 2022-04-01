db2157af01b3a60f6325f420c2c0fadfce1db058
Slade Getz <asgetz@mines.edu>
2022-04-01 14:46:44 -0600

    fixed symbolic link

--- sudo_tid.sh
+++ sudo_tid.sh
@@ -1,1 +1,19 @@
-../../myScripts/sudo_tid.sh
+#!/bin/zsh
+
+
+declare myID
+myID="$(id -u)"
+
+if [[ $myID -ne 0 ]]
+then
+  echo "SUDO required"
+  exit
+fi
+
+if grep -q "pam_tid.so" /etc/pam.d/sudo; then
+  echo "Touch ID already set";
+else
+  sed -i '' '1a\
+  auth       sufficient     pam_tid.so\
+  ' /etc/pam.d/sudo;
+fi
